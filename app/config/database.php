dcc192cc36953c35b2d4a6ade5f0e7f1b138e98f
Mayllon Veras <mayllonverasu@gmail.com>
2017-11-28 14:20:05 -0300

    first commit

---
+++ app/config/database.php
@@ -0,0 +1,22 @@
+<?php
+class DATABASE_CONFIG {
+        /*
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'parnaiba_recadastramento',
+	);
+        */
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'bm94.webservidor.net',
+		'login' => 'th141281_recad1',
+		'password' => 'c8PsLeAsGn',
+		'database' => 'th141281_recadastramentopmp',
+	);
+}
+?>
